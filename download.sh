7e2ff62ae576b769227c418c9f460053079e6796
ShantanuWelling <shantanuwelling@gmail.com>
2024-10-24 15:28:38 +0530

    download traces script

---
+++ download.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/603.bwaves_s-891B.champsimtrace.xz
+#wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/605.mcf_s-472B.champsimtrace.xz
+#wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/607.cactuBSSN_s-2421B.champsimtrace.xz
+#wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/619.lbm_s-2676B.champsimtrace.xz
+wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/620.omnetpp_s-141B.champsimtrace.xz > /dev/null
+wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/621.wrf_s-575B.champsimtrace.xz > /dev/null
+wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/623.xalancbmk_s-10B.champsimtrace.xz > /dev/null
+wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/649.fotonik3d_s-1B.champsimtrace.xz > /dev/null
+wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/654.roms_s-293B.champsimtrace.xz > /dev/null
+wget https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/602.gcc_s-734B.champsimtrace.xz  > /dev/null
