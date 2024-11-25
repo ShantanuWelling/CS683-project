2abbbcda901df9208a8e008d015733848f9662bd
ShantanuWelling <shantanuwelling@gmail.com>
2024-11-25 19:01:05 +0530

    pmp pdr

--- PMP/run_trace.sh
+++ PMP/run_trace.sh
@@ -1,10 +1,10 @@
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../602.gcc_s-734B.champsimtrace.xz > logs/metrics/602.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../603.bwaves_s-891B.champsimtrace.xz > logs/metrics/603.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../605.mcf_s-472B.champsimtrace.xz > logs/metrics/605.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../607.cactuBSSN_s-2421B.champsimtrace.xz > logs/metrics/607.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../619.lbm_s-2676B.champsimtrace.xz > logs/metrics/619.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../620.omnetpp_s-141B.champsimtrace.xz > logs/metrics/620.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../621.wrf_s-575B.champsimtrace.xz > logs/metrics/621.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../623.xalancbmk_s-10B.champsimtrace.xz > logs/metrics/623.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../649.fotonik3d_s-1B.champsimtrace.xz > logs/metrics/649.txt
-./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../654.roms_s-293B.champsimtrace.xz > logs/metrics/654.txt
+# ./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../602.gcc_s-734B.champsimtrace.xz > logs/pmp_pdr/602.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/603.bwaves_s-891B.champsimtrace.xz > logs/pmp_pdr/603.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/605.mcf_s-472B.champsimtrace.xz > logs/pmp_pdr/605.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/607.cactuBSSN_s-2421B.champsimtrace.xz > logs/pmp_pdr/607.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/619.lbm_s-2676B.champsimtrace.xz > logs/pmp_pdr/619.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/620.omnetpp_s-141B.champsimtrace.xz > logs/pmp_pdr/620.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/621.wrf_s-575B.champsimtrace.xz > logs/pmp_pdr/621.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/623.xalancbmk_s-10B.champsimtrace.xz > logs/pmp_pdr/623.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/649.fotonik3d_s-1B.champsimtrace.xz > logs/pmp_pdr/649.txt &
+./bin/bimodal-no-pmp-no-no-lru-1core -warmup_instructions 50000000 -simulation_instructions 200000000 -traces ../traces/654.roms_s-293B.champsimtrace.xz > logs/pmp_pdr/654.txt &
